cf921a714f3d59be3eca5dc9180fde54f7eefa68
Talita <talitamarcelo15@gmail.com>
2021-09-15 14:18:37 -0300

    Adicionar arquivos de projeto.

---
+++ circulo].cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace circulo
+{
+    class circulo
+    {
+        static void Main(string[] args)
+        {
+            // declrando a classe
+            Calculadora calc = new Calculadora();
+
+            // interaçao com usuario
+            Console.WriteLine("Entre com o valor do raio: ");
+            // montando o calculo
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = calc.Circuferencia(raio);
+            double volume = calc.Volume(raio);
+
+            // mostra o resultado
+            Console.WriteLine("Circuferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de Pi: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadKey();
+        }
+    }
+}
